2ba35f466446f6522cfa27ec58b2b6c4c194cc7a
mac <mac@macdeMacBook-Pro.local>
2020-12-15 16:40:29 -0500

    final commit

--- test.R
+++ test.R
@@ -1,8 +1,16 @@
 library(testthat)
 
 context("check forward function")
-source("forward_function.R")
+source("/Users/mac/final-project/forward_function.R")
 
 test_that("error thrown when input is number", {
   expect_error(selection_function(0, 0, 0))
+})
+
+test_that("error thrown when full data is missing", {
+  expect_error(selection_function(respons_e, begin_data))
+})
+
+test_that("error thrown when respons_e is not vector", {
+  expect_error((selection_function(data.frame("a" = 1, "b" = 2), data.frame(x), data.frame(y))))
 })
